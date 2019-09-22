272f4d79fca848a9a7943c62b30d340c8250d67a
Arto Ra <52731390+Artora@users.noreply.github.com>
2019-09-22 12:53:11 +0800

    1.7 Python debugging done

--- debugging/pdb_ex.py
+++ debugging/pdb_ex.py
@@ -9,22 +9,24 @@ random2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 while True:
     print("To exit this game type 'exit'")
 
-    pdb.set_trace()
+    #pdb.set_trace()
 
-    answer = input("What is {} times {}? ".format(
-        choice(random2), choice(random1)))
+    num1 = choice(random2)
+    num2 = choice(random1)
+    answer = int(input("What is {} times {}? ".format(
+        num1, num2)))
 
     # exit
     if answer == "exit":
         print("Now exiting game!")
         sys.exit()
 
-    test = int(choice(random2))*(choice(random1))
+    #test = int(choice(random2))*(choice(random1))
 
     # determine if number is correct
-    #elif answer == choice(random2) * choice(random1):
-    #    print("Correct!")
-    #else:
-    #    print("Wrong!")
+    elif answer == num1 * num2:
+        print("Correct!")
+    else:
+        print("Wrong!")
 
 
