0fecabb629a5acd6bd1a11b0ed89b7f3e7ab4170
Arto Ra <52731390+Artora@users.noreply.github.com>
2019-10-02 20:52:05 +0800

    Error Handling

--- flasktaskr/views.py
+++ flasktaskr/views.py
@@ -11,6 +11,8 @@ from forms import AddTaskForm, RegisterForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
+from sqlalchemy.exc import IntegrityError
+
 # config
 
 app = Flask(__name__)
@@ -245,10 +247,15 @@ def register():
                 form.email.data,
                 form.password.data
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', \
+                    form=form, error=error)
     return render_template('register.html', form = form, \
         error = error)
 
