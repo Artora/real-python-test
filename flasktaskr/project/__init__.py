2c4d4d53bae5d1c2ce43856486228a5a79acb491
Arto Ra <52731390+Artora@users.noreply.github.com>
2019-11-21 22:00:31 +0800

    new features

--- flasktaskr/project/__init__.py
+++ flasktaskr/project/__init__.py
@@ -1,11 +1,12 @@
 # project/__init__.py
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
+bcrypt = Bcrypt(app)
 db =SQLAlchemy(app)
 
 from project.users.views import users_blueprint
@@ -16,3 +17,6 @@ app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
