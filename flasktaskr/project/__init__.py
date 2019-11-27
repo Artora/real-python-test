c45375b5adda39256731e0209768ceb5c9a0b4d9
Arto Ra <52731390+Artora@users.noreply.github.com>
2019-11-27 12:34:47 +0800

    new features

--- flasktaskr/project/__init__.py
+++ flasktaskr/project/__init__.py
@@ -1,8 +1,9 @@
 # project/__init__.py
 
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+import datetime
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
@@ -19,10 +20,23 @@ app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\404 error at {}: {} ".format(current_timestamp, r))
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n500 error at {}: {} ".format(current_timestamp, r))
     return render_template('500.html'), 500
 
     
